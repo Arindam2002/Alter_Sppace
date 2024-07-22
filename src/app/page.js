8d75ab3e3f50d51effa0d04a6797cdf25dbc4682
rajutkarsh07 <rajutkarsh2505@gmail.com>
2024-07-23 01:02:47 +0530

    made testimonial and main content component

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,109 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
-import TestimonialCard from '@/components/TestimonialCard';
+import testimonials from './testimonials.json';
+import TestimonialCard from '@/components/Testimonial/TestimonialCard';
+
+import mainimg from '../../public/assets/mainimg.png';
 
 export default function Home() {
-  const testimonials = [
-    {
-      img: '/path/to/image1.jpg',
-      name: 'John Doe',
-      address: 'New York, NY',
-      testimonial: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-    },
-    {
-      img: '/path/to/image2.jpg',
-      name: 'Jane Smith',
-      address: 'Los Angeles, CA',
-      testimonial: `It is a long established fact that a reader will be distracted by the readable 
-      content of a page when looking at its layout.`,
-    },
-    {
-      img: '/path/to/image3.jpg',
-      name: 'Alice Johnson',
-      address: 'Chicago, IL',
-      testimonial: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots 
-      in a piece of classical Latin literature from 45 BC.`,
-    },
-  ];
-
   return (
-    <div className="space-y-8">
-      {testimonials.map((testimonial, index) => (
-        <TestimonialCard
-          key={index}
-          img={testimonial.img}
-          name={testimonial.name}
-          address={testimonial.address}
-          testimonial={testimonial.testimonial}
-        />
-      ))}
+    <div>
+      <div className="main-content">
+        <img src={mainimg.src} alt="mainimg" className="main-image" />
+
+        <div className="main-description">
+          <h2>
+            We are professionals with an average of more than 15 yrs. of
+            experience
+          </h2>
+          <p>
+            We are a group of professionals with an average of more than 15 yrs.
+            of experience in SERVICE INDUSTRY in Indian as well as in
+            international market. In a short span of time we have accumulated an
+            impressive track record in this region of credibility and trust. To
+            reach out to more and more clients we have been investing in
+            grooming several interior designers and designing enthusiastic. We
+            firmly believe that the interior design industry in India is in its
+            nascent stage and will grow drastically and AlterSpace hope to
+            contribute immensely to the development of this industry as we build
+            on our credentials and accolades. Our team understands the
+            challenges of the home decor industry, especially balancing the
+            three most important pillars: Budget, Delivery Schedule, and
+            Creativity with durability.
+          </p>
+
+          <div className="pillars-container">
+            <div className="pillar">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    d="M9.108 12.258L11.392 9.96797L9.339 7.90797L8.18 9.06597L7.472 8.35797L8.625 7.19997L6.673 5.24797L4.383 7.53797L9.108 12.258ZM16.942 20.098L19.232 17.808L17.281 15.856L16.123 17.008L15.416 16.3L16.567 15.142L14.527 13.108L12.242 15.392L16.942 20.098ZM7.153 20.5H4V17.346L8.394 12.952L3 7.53897L6.673 3.86597L12.112 9.28497L16.579 4.81197C16.689 4.7013 16.805 4.62163 16.927 4.57297C17.049 4.5243 17.179 4.49997 17.317 4.49997C17.455 4.49997 17.585 4.5243 17.707 4.57297C17.829 4.62163 17.9453 4.7013 18.056 4.81197L19.688 6.50797C19.7987 6.61797 19.8753 6.73397 19.918 6.85597C19.9607 6.97797 19.9817 7.10797 19.981 7.24597C19.9803 7.38397 19.959 7.50997 19.917 7.62397C19.875 7.73797 19.799 7.84997 19.689 7.95997L15.259 12.414L20.635 17.827L16.96 21.5L11.547 16.106L7.153 20.5ZM5 19.5H6.727L16.547 9.68597L14.813 7.95397L5 17.773V19.5ZM15.692 8.81397L14.814 7.95397L16.546 9.68697L15.692 8.81397Z"
+                    fill="#F8952D"
+                  />
+                </svg>
+              </div>
+              <p className="pillar-text">Creativity</p>
+            </div>
+            <div className="pillar">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    d="M9.108 12.258L11.392 9.96797L9.339 7.90797L8.18 9.06597L7.472 8.35797L8.625 7.19997L6.673 5.24797L4.383 7.53797L9.108 12.258ZM16.942 20.098L19.232 17.808L17.281 15.856L16.123 17.008L15.416 16.3L16.567 15.142L14.527 13.108L12.242 15.392L16.942 20.098ZM7.153 20.5H4V17.346L8.394 12.952L3 7.53897L6.673 3.86597L12.112 9.28497L16.579 4.81197C16.689 4.7013 16.805 4.62163 16.927 4.57297C17.049 4.5243 17.179 4.49997 17.317 4.49997C17.455 4.49997 17.585 4.5243 17.707 4.57297C17.829 4.62163 17.9453 4.7013 18.056 4.81197L19.688 6.50797C19.7987 6.61797 19.8753 6.73397 19.918 6.85597C19.9607 6.97797 19.9817 7.10797 19.981 7.24597C19.9803 7.38397 19.959 7.50997 19.917 7.62397C19.875 7.73797 19.799 7.84997 19.689 7.95997L15.259 12.414L20.635 17.827L16.96 21.5L11.547 16.106L7.153 20.5ZM5 19.5H6.727L16.547 9.68597L14.813 7.95397L5 17.773V19.5ZM15.692 8.81397L14.814 7.95397L16.546 9.68697L15.692 8.81397Z"
+                    fill="#F8952D"
+                  />
+                </svg>
+              </div>
+              <p className="pillar-text">Durability</p>
+            </div>
+            <div className="pillar">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    d="M9.108 12.258L11.392 9.96797L9.339 7.90797L8.18 9.06597L7.472 8.35797L8.625 7.19997L6.673 5.24797L4.383 7.53797L9.108 12.258ZM16.942 20.098L19.232 17.808L17.281 15.856L16.123 17.008L15.416 16.3L16.567 15.142L14.527 13.108L12.242 15.392L16.942 20.098ZM7.153 20.5H4V17.346L8.394 12.952L3 7.53897L6.673 3.86597L12.112 9.28497L16.579 4.81197C16.689 4.7013 16.805 4.62163 16.927 4.57297C17.049 4.5243 17.179 4.49997 17.317 4.49997C17.455 4.49997 17.585 4.5243 17.707 4.57297C17.829 4.62163 17.9453 4.7013 18.056 4.81197L19.688 6.50797C19.7987 6.61797 19.8753 6.73397 19.918 6.85597C19.9607 6.97797 19.9817 7.10797 19.981 7.24597C19.9803 7.38397 19.959 7.50997 19.917 7.62397C19.875 7.73797 19.799 7.84997 19.689 7.95997L15.259 12.414L20.635 17.827L16.96 21.5L11.547 16.106L7.153 20.5ZM5 19.5H6.727L16.547 9.68597L14.813 7.95397L5 17.773V19.5ZM15.692 8.81397L14.814 7.95397L16.546 9.68697L15.692 8.81397Z"
+                    fill="#F8952D"
+                  />
+                </svg>
+              </div>
+              <p className="pillar-text">Budget Friendly</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-details"></div>
+
+      <div className="testimonial">
+        <h2 className="testimonial-heading">What the People Thinks About Us</h2>
+        <div className="testimonial-cards">
+          {testimonials.map((testimonial, index) => (
+            <TestimonialCard
+              key={index}
+              img={testimonial.img}
+              name={testimonial.name}
+              address={testimonial.address}
+              testimonial={testimonial.testimonial}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Testimonial/TestimonialCard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import './TestimonialCard.scss';
+
+const TestimonialCard = ({ img, name, address, testimonial }) => {
+  return (
+    <div className="testimonial-card">
+      <div className="testimonial-card-header">
+        <img src={img.src} alt="user" className="testimonial-card-img" />
+
+        <div className="testimonial-card-info">
+          <h2 className="testimonial-card-name">{name}</h2>
+          <p className="testimonial-card-address">{address}</p>
+        </div>
+      </div>
+      <p className="testimonial-card-text">{testimonial}</p>
+    </div>
+  );
+};
+
+export default TestimonialCard;
